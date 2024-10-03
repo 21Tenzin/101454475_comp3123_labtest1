7c16fc9c823262a1d29b97a60996cffda71e2448
Tenzin Thinley <tenzint21@gmail.com>
2024-10-03 16:07:06 -0400

    question 2 complete

---
+++ question-2/callbacks.js
@@ -0,0 +1,27 @@
+function resolvedPromise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({message: 'delayed success!'});
+        }, 500);   
+    });
+}
+
+function rejectedPromise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject({error: 'delayed exception!'});   
+        }, 500);   
+    });
+}
+
+resolvedPromise().then((result) => {
+    console.log(result);
+}).catch((error) => {
+    console.log(error);
+});
+
+rejectedPromise().then((result) => {
+    console.log(result);
+}).catch((error) => {
+    console.log(error);
+});
